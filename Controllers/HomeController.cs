1c1ea5b8b5197d603f495b1c4343fc6e19d70c44
Murilo Rerison de Sousa Paz <murilokapaz@gmail.com>
2019-11-08 15:47:30 -0300

    alteracao

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -14,13 +14,13 @@ namespace ProjetoWeb.Controllers {
 
         public IActionResult About() {
             ViewData["Message"] = "Your application description page.";
-
+            
             return View();
         }
 
         public IActionResult Contact() {
             ViewData["Message"] = "Your contact page.";
-
+            ViewData["Student"] = "Murilo Rerison";
             return View();
         }
 
