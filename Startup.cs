085f9c536752d59976cf98da4fbbdfe6793002cb
Murilo Rerison de Sousa Paz <murilokapaz@gmail.com>
2019-11-17 01:23:03 -0300

    first Migration

--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using ProjetoWeb.Models;
 
 namespace ProjetoWeb {
     public class Startup {
@@ -28,6 +30,9 @@ namespace ProjetoWeb {
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+    services.AddDbContext<ProjetoWebContext>(options =>
+            options.UseMySql(Configuration.GetConnectionString("ProjetoWebContext"), builder=> builder.MigrationsAssembly("ProjetoWeb")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
